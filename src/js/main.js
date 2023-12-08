11e60cb32255c899bcccf6e18cc1957ddfa6d54c
Den <den204d@gmail.com>
2023-12-08 16:50:28 +0200

    add contact and footer section. And make them adaptive to all device. Fix main.js

--- src/js/main.js
+++ src/js/main.js
@@ -4,22 +4,38 @@ listNumber.forEach((n, index) => {
   n.setAttribute('data-before', "0" + (index + 1));
 })
 
-const listLine = document.querySelectorAll(".clients__list li");
 
-listLine.forEach((element, index) => {
-  if (screen.width > 1200) {
+function borderList() {
+  const listLine = document.querySelectorAll(".clients__list li");
+  listLine.forEach((element, index) => {
+    if (window.innerWidth > 1200) {
+      if (index !== 5) {
+        element.style.borderBottom = "none";
+      }
+      if (index <= 2) {
+        element.style.borderBottom = "1px solid #000";
+      }
 
-    if (index <= 2) {
-      element.style.borderBottom = "1px solid #000";
+      if (index == 1 || index == 4) {
+        element.style.borderRight = "1px solid  #000";
+        element.style.borderLeft = "1px solid #000";
+      }
+    } else {
+      if (index <= 2) {
+        element.style.borderBottom = "none";
+      }
+      if (index == 1 || index == 4) {
+        element.style.borderRight = "none";
+        element.style.borderLeft = "none";
+      }
+      if (index !== 5) {
+        element.style.borderBottom = "1px solid #000";
+      }
     }
 
-    if (index == 1 || index == 4) {
-      element.style.borderRight = "1px solid  #000";
-      element.style.borderLeft = "1px solid #000";
-    }
-  } else {
-    if (index !== 5) {
-      element.style.borderBottom = "1px solid #000";
-    }
-  }
-});
+  });
+}
+borderList();
+
+document.onload = borderList;
+window.onresize = borderList;
